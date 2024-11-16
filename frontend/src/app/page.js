8082629d8526cbfb16fe93669d2a611fb3b90300
mohithn2004 <mohithn2004@gmail.com>
2024-11-16 15:43:42 +0530

    added signup page

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,13 +2,15 @@ import React from 'react'
 import Home from './components/HomePage'
 import Notification from './components/Notification'
 import LoginForm from './components/LoginPage'
+import SignUpPage from './components/SignUpPage'
 
 function page() {
   return (
     <div>
       {/* <Home/> */}
       {/* <Notification/> */}
-      <LoginForm/>
+      {/* <LoginForm/> */}
+      <SignUpPage/>
     </div>
   )
 }
