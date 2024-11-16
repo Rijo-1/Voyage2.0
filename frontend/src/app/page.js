188c436777357f7623c3b38309441e92955d1f47
mohithn2004 <mohithn2004@gmail.com>
2024-11-16 11:32:38 +0530

    updated frontend

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -4,8 +4,9 @@ import background from "./media/background.gif"
 export default function Home() {
   return (
     <>
-      <Image className="h-lvh w-lvw" src={background} alt="bg"/>
+      <Image className=" absolute h-lvh w-lvw" src={background} alt="bg"/>
       <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-black bg-fixed opacity-45"></div>
+      <h1 className="absolute text-rose-100">Voyage</h1>
     </>
   );
 }
